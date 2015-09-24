ef743233d7a2a385bd58eefe49171c556926a4f4
sitthidate nanuea <sitthidate@moctis.com>
2015-09-24 14:18:07 +0700

    Update seed.js

--- seed.js
+++ seed.js
@@ -3,4 +3,9 @@ $eed
   .append('Hello, Moctis<br/>')
   .append('Hello, Benze<br/>')
   .append('Hello, Link<br/>')
+  .append('Hello, Wut<br/>')
+  .append('Hello World<br/>')
+  .append('Hello, Moctis<br/>')
+  .append('Hello, Benze<br/>')
+  .append('Hello, Link<br/>')
   .append('Hello, Wut<br/>');
